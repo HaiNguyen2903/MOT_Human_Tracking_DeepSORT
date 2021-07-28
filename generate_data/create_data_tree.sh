5c40290a25a26bbf7036c8862a16e7b6dfea7b44
HaiNguyen2903 <hainguyen29031412@gmail.com>
2021-07-28 09:25:30 +0700

    add combine train data code and create data tree sh

---
+++ generate_data/create_data_tree.sh
@@ -0,0 +1,19 @@
+cd ..
+
+mkdir combine_dataset
+
+cd combine_dataset
+
+mkdir images labels
+
+cd images
+
+mkdir train val
+
+cd ../labels
+
+mkdir train val
+
+cd ../../generate_data
+
+python combine_train_data.py
