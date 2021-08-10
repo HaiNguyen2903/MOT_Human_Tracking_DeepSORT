8926dd845f2fff6632607bb51f0f700bb8f963fa
HaiNguyen2903 <hainguyen29031412@gmail.com>
2021-08-10 12:34:22 +0700

    adding visualize top k and custom dataloader

--- deep_sort_pytorch/deep_sort/deep/evaluate.py
+++ deep_sort_pytorch/deep_sort/deep/evaluate.py
@@ -1,11 +1,17 @@
 import torch
 from IPython import embed
 import argparse
+import os
+import cv2
+import numpy as np
 
 parser = argparse.ArgumentParser(description="Train on market1501")
-parser.add_argument("--predict_path", default='predicts/features_train25epochs.pth', type=str)
+parser.add_argument("--predict_path", default='predicts/features_new.pth', type=str)
 parser.add_argument("--p_k", default=5, type=int)
 parser.add_argument("--map_n", default=5, type=int)
+parser.add_argument("--visualize_rank_k", default=10, type=int)
+parser.add_argument("--inference_dir", default = "inference", type=str)
+
 args = parser.parse_args()
 
 
@@ -23,20 +29,19 @@ ql = features["ql"]
 gf = features["gf"]
 gl = features["gl"]
 
+query_paths = features['query_paths']
+gallery_paths = features['gallery_paths']
+
 # matrix of confidence with shape (querry frames x gallery frames)
 scores = qf.mm(gf.t())
 
-# # return vector of index in scores metric that have highest score for each querry: len = querry frame
-# res = scores.topk(5, dim=1)[1][:, 0]            
-
-# # total equal values between ql and ql vectors
-# top1correct = gl[res].eq(ql).sum().item()
-
-# print("Accuracy top 1: {:.3f}".format(top1correct / ql.size(0)))
-
 def calculate_rank_1(scores):
+    # return vector of index in scores metric that have highest score for each querry: len = querry frame
     res = scores.topk(5, dim=1)[1][:, 0]
+
+    # total equal values between ql and ql vectors
     top1correct = gl[res].eq(ql).sum().item()
+    
     print("Accuracy top 1: {:.3f}".format(top1correct / ql.size(0)))
     return top1correct / ql.size(0)
 
@@ -121,7 +126,94 @@ def calculate_mAP_n(scores, n=5):
     return mAP
 
 
+'''
+Visualize rank k result for each query
+'''
+
+GRID_SPACING = 10
+QUERY_EXTRA_SPACING = 90
+BW = 5 # border width
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+
+def mkdir_if_missing(path):
+    if not os.path.exists(path):
+        print('Make dir {}'.format(path))
+        os.mkdir(path)
+
+
+def visualize_rank_k(scores, output_dir, width=128, height=256, topk=10):
+    mkdir_if_missing(output_dir)
+
+    # return vector of index in scores metric that have highest score for each querry: len = querry frame
+    indices = scores.topk(topk, dim=1)[1]
+
+    # top k pred for each query, return matrix of (number query x n)
+    pred = gl[indices]
+
+    for i in range(ql.size(0)): 
+        qimg = cv2.imread(query_paths[i])
+        qimg = cv2.resize(qimg, (width, height))
+        qimg = cv2.copyMakeBorder(
+            qimg, BW, BW, BW, BW, cv2.BORDER_CONSTANT, value=(0, 0, 0)
+        )
+
+        # resize twice to ensure that the border width is consistent across images
+        qimg = cv2.resize(qimg, (width, height))
+
+        num_cols = topk + 1
+        grid_img = 255 * np.ones(
+            (
+                height,
+                num_cols*width + topk*GRID_SPACING + QUERY_EXTRA_SPACING, 3
+            ),
+            dtype=np.uint8
+        )
+        grid_img[:, :width, :] = qimg
+
+
+        # make subdir for each query
+        pid = ql[i].item()
+        mkdir_if_missing(os.path.join(output_dir, str(pid)))
+
+        # list of matched predicts (boolean tensor)
+        matches = pred[i].eq(ql[i])
+
+        rank_idx = 1
+        embed()
+        for j in range(len(matches)):
+            border_color = GREEN if matches[j] else RED
+            gimg = cv2.imread(gallery_paths[indices[i][j].item()])
+            gimg = cv2.resize(gimg, (width, height))
+            gimg = cv2.copyMakeBorder(
+                        gimg,
+                        BW,
+                        BW,
+                        BW,
+                        BW,
+                        cv2.BORDER_CONSTANT,
+                        value=border_color
+                    )
+            gimg = cv2.resize(gimg, (width, height))
+            start = rank_idx*width + rank_idx*GRID_SPACING + QUERY_EXTRA_SPACING
+            end = (
+                rank_idx+1
+            ) * width + rank_idx*GRID_SPACING + QUERY_EXTRA_SPACING
+            grid_img[:, start:end, :] = gimg
+
+            rank_idx += 1
+            if rank_idx > topk:
+                break
+
+    
+        imname = os.path.basename(query_paths[i])[:-4]
+        cv2.imwrite(os.path.join(output_dir, str(pid), imname + '.jpg'), grid_img)
+        print('write {}'.format(imname))
+
 if __name__ == '__main__':
     calculate_rank_1(scores)
     calculate_precision_k(scores, args.p_k)
     calculate_mAP_n(scores, args.map_n)
+    visualize_rank_k(scores, args.inference_dir)
+
+
--- deep_sort_pytorch/deep_sort/deep/test.py
+++ deep_sort_pytorch/deep_sort/deep/test.py
@@ -1,4 +1,4 @@
-
+# from deep_sort_pytorch.deep_sort.deep.custom_dataloader import ImageFolderWithPaths
 import torch
 import torch.backends.cudnn as cudnn
 import torchvision
@@ -9,16 +9,19 @@ import os
 from torchvision.datasets.folder import default_loader
 from traitlets.traitlets import default
 
+from custom_dataloader import ImageFolderWithPaths
+
 from model import Net
 from IPython import embed
 
+
 parser = argparse.ArgumentParser(description="Train on market1501")
-parser.add_argument("--data-dir", default='data', type=str)
+parser.add_argument("--data-dir", default='/data.local/hangd/data_vtx/reid_dataset/uet_reid', type=str)
 parser.add_argument("--no-cuda", action="store_true")
 parser.add_argument("--gpu-id", default=1, type=int)
 parser.add_argument("--ckpt", default="./checkpoint/ckpt.t7", type=str)
 parser.add_argument("--batch", default=8, type=int)
-parser.add_argument("--save-path", default="predicts/features_original.pth", type=str)
+parser.add_argument("--save-path", default="predicts/features_new.pth", type=str)
 
 args = parser.parse_args()
 
@@ -36,23 +39,28 @@ if torch.cuda.is_available() and not args.no_cuda:
 root = args.data_dir
 query_dir = os.path.join(root, "query")
 gallery_dir = os.path.join(root, "gallery")
+
 transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize((128, 64)),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize(
         [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
+
+
 queryloader = torch.utils.data.DataLoader(
-    torchvision.datasets.ImageFolder(query_dir, transform=transform),
+    ImageFolderWithPaths(query_dir, transform=transform),
     batch_size=args.batch, shuffle=False
 )
 '''
 try load gallery random
 '''
 galleryloader = torch.utils.data.DataLoader(
-    torchvision.datasets.ImageFolder(gallery_dir, transform=transform),
+    ImageFolderWithPaths(gallery_dir, transform=transform),
     batch_size=args.batch, shuffle=True
 )
+
+embed()
 
 # net definition
 net = Net(reid=True)
@@ -79,11 +87,15 @@ net.to(device)
 # compute features
 query_features = torch.tensor([]).float()
 query_labels = torch.tensor([]).long()
+query_paths = []
+
 gallery_features = torch.tensor([]).float()
 gallery_labels = torch.tensor([]).long()
+gallery_paths = []
+
 
 with torch.no_grad():
-    for idx, (inputs, labels) in enumerate(queryloader):
+    for idx, (inputs, labels, paths) in enumerate(queryloader):
 
         inputs = inputs.to(device)
 
@@ -91,14 +103,18 @@ with torch.no_grad():
 
         query_features = torch.cat((query_features, features), dim=0)
         query_labels = torch.cat((query_labels, labels))
+        query_paths.extend(paths)
+
         # embed(header='query')
 
-    for idx, (inputs, labels) in enumerate(galleryloader):
+    for idx, (inputs, labels, paths) in enumerate(galleryloader):
         inputs = inputs.to(device)
         features = net(inputs).cpu()
 
         gallery_features = torch.cat((gallery_features, features), dim=0)
         gallery_labels = torch.cat((gallery_labels, labels))
+        gallery_paths.extend(paths)
+
         # embed(header='gallery')
 
 # gallery_labels -= 2
@@ -108,6 +124,9 @@ features = {
     "qf": query_features,
     "ql": query_labels,
     "gf": gallery_features,
-    "gl": gallery_labels
+    "gl": gallery_labels,
+    "query_paths": query_paths,
+    "gallery_paths": gallery_paths
 }
+
 torch.save(features, args.save_path)
